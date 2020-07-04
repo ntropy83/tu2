5c53a9ee7e041676e6e0691aa01ed4c00aa21fbc
enthalpie <o.sagner@gmx.de>
2020-07-04 22:02:16 +0200

    Save Path fix and new .desktop file

--- inc/save_handler.hpp
+++ inc/save_handler.hpp
@@ -29,10 +29,9 @@ struct date_sort {
     static QString prjdir = "data/";
     static QString lib_index = "index.lib";
 #else
-    static QString home = QDir::homePath();
-    static QString maindir = home + "/usr/lib/tu2//Projekte/";
-    static QString prjdir = home + "/usr/lib/tu2/TUII/data/";
-    static QString lib_index = home + "/usr/lib/tu2/index.lib";
+    static QString maindir = "/usr/lib/tu2//Projekte/";
+    static QString prjdir = "/usr/lib/tu2/TUII/data/";
+    static QString lib_index = "/usr/lib/tu2/index.lib";
 #endif
 
 // Zählt die Anzahl der Projekte und inkrementiert sie bei Neuanlage
